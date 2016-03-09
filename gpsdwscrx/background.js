02d8d88dac52babc2cd5f25ab2fe8d453d69adc9
Michael Farrell <micolous+git@gmail.com>
2016-03-10 09:53:09 +1100

    add connection to gpspipe

--- gpsdwscrx/background.js
+++ gpsdwscrx/background.js
@@ -1,5 +1,22 @@
-var port = chrome.runtime.connect();
+var gpsd_port = null
 
+gpsd_port_messageHandler = function(msg) {
+	console.log('gpsd: ' + JSON.stringify(msg));
+};
+
+gpsd_port_disconnectHandler = function(first_try) {
+	if (first_try !== true) {
+		console.log('gpsd: disconnected, trying to reconnect in 1s...');
+	}
+
+	setTimeout(function() {
+		gpsd_port = chrome.runtime.connectNative('au.id.micolous.gpspipe');
+		gpsd_port.onDisconnect.addListener(gpsd_port_disconnectHandler);
+		gpsd_port.onMessage.addListener(gpsd_port_messageHandler);
+	}, 1000);
+};
+
+gpsd_port_disconnectHandler(true);
 
 
 chrome.browserAction.onClicked.addListener(function(tab) {
